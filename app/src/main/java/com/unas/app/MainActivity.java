e05b6068a3c1da8030fb181531ff640dc738c430
FabianConP <fabian.conpir@gmail.com>
2014-09-11 21:34:56 -0500

    Initial commit

---
+++ app/src/main/java/com/unas/app/MainActivity.java
@@ -0,0 +1,53 @@
+package com.unas.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+
+
+public class MainActivity extends Activity implements AnimationListener{
+
+    Animation animWelcome;
+    FrameLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        animWelcome = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.welcome_animation);
+        animWelcome.setAnimationListener(this);
+        container = (FrameLayout)(findViewById(R.id.welcomeContainer));
+        container.startAnimation(animWelcome);
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Take any action after completing the animation
+
+        // check for fade in animation
+        if (animation == animWelcome) {
+            //Toast.makeText(getApplicationContext(), "Animation Stopped",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(),universeActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ app/src/main/java/com/unas/app/universeActivity.java
@@ -0,0 +1,15 @@
+package com.unas.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class universeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_universe);
+    }
+
+}
