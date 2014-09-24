4c39539a67e1b994eb502b468c77e17937449170
FabianConP <fabian.conpir@gmail.com>
2014-09-23 23:07:00 -0500

    Botones de Zoom In y Out funcionales

--- app/src/main/java/com/unas/app/universeActivity.java
+++ app/src/main/java/com/unas/app/universeActivity.java
@@ -2,47 +2,104 @@ package com.unas.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class universeActivity extends Activity implements AnimationListener {
 
-    int[] potenciasP = {
+    int[] powersImage = {
             R.drawable.pot7,
             R.drawable.pot8,
             R.drawable.pot9,
             R.drawable.pot10
     };
 
-    Animation scaleLow;
-    Animation scaleIn;
-    Animation scaleUp;
-    Animation scaleOut;
-    ImageView imageOut;
-    ImageView imageIn;
+    Animation scaleFront;
+    Animation scaleBack;
+    ImageView imageFront;
+    ImageView imageBack;
     int scale = 0;
+
+    Button btZoomIn;
+    Button btZoomOut;
+    Button btInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universe);
-        imageOut = (ImageView) (findViewById(R.id.first));
-        imageIn = (ImageView) (findViewById(R.id.second));
-        imageIn.setImageResource(potenciasP[scale + 1]);
-        imageOut.setImageResource(potenciasP[scale]);
+        btZoomIn = (Button) (findViewById(R.id.btZoomIn));
+        btZoomOut = (Button) (findViewById(R.id.btZoomOut));
+        btInfo = (Button) (findViewById(R.id.btInfo));
+        scale = 0;
 
+        imageFront = (ImageView) (findViewById(R.id.first));
+        imageBack = (ImageView) (findViewById(R.id.second));
 
-        scaleLow = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_low);
-        scaleLow.setAnimationListener(this);
-        imageOut.startAnimation(scaleLow);
-
-        scaleIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_in2);
-        scaleIn.setAnimationListener(this);
-        imageIn.startAnimation(scaleIn);
+        imageFront.setImageResource(powersImage[scale]);
     }
+
+    public void onClickZoomIn(View v) {
+        if (scale - 1 >= 0) {
+            btZoomOut.setVisibility(View.VISIBLE);
+
+            if (scale % 2 == 0) {
+                imageFront = (ImageView) (findViewById(R.id.first));
+                imageBack = (ImageView) (findViewById(R.id.second));
+            } else {
+                imageBack = (ImageView) (findViewById(R.id.first));
+                imageFront = (ImageView) (findViewById(R.id.second));
+            }
+
+            imageFront.setImageResource(powersImage[scale]);
+            imageBack.setImageResource(powersImage[scale - 1]);
+
+            scaleBack = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_in_front);
+            scaleBack.setAnimationListener(this);
+            imageFront.startAnimation(scaleBack);
+
+            scaleBack = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_in_back);
+            scaleBack.setAnimationListener(this);
+            imageBack.startAnimation(scaleBack);
+            scale--;
+            if (scale == 0)
+                btZoomIn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void onClickZoomOut(View v) {
+        if (scale + 1 < powersImage.length) {
+            btZoomIn.setVisibility(View.VISIBLE);
+
+            if (scale % 2 == 0) {
+                imageFront = (ImageView) (findViewById(R.id.first));
+                imageBack = (ImageView) (findViewById(R.id.second));
+            } else {
+                imageBack = (ImageView) (findViewById(R.id.first));
+                imageFront = (ImageView) (findViewById(R.id.second));
+            }
+
+            imageFront.setImageResource(powersImage[scale]);
+            imageBack.setImageResource(powersImage[scale + 1]);
+
+            scaleFront = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_low_front);
+            scaleFront.setAnimationListener(this);
+            imageFront.startAnimation(scaleFront);
+
+            scaleBack = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_low_back);
+            scaleBack.setAnimationListener(this);
+            imageBack.startAnimation(scaleBack);
+            scale++;
+            if (scale + 1 == powersImage.length)
+                btZoomOut.setVisibility(View.INVISIBLE);
+        }
+    }
+
 
     @Override
     public void onAnimationStart(Animation animation) {
@@ -51,42 +108,11 @@ public class universeActivity extends Activity implements AnimationListener {
 
     @Override
     public void onAnimationEnd(Animation animation) {
-        if (animation == scaleIn && scale < 2) {
-            scale++;
-            if (scale % 2 == 0) {
-                imageOut = (ImageView) (findViewById(R.id.first));
-                imageIn = (ImageView) (findViewById(R.id.second));
-            } else {
-                imageIn = (ImageView) (findViewById(R.id.first));
-                imageOut = (ImageView) (findViewById(R.id.second));
-            }
-
-            imageIn.setImageResource(potenciasP[scale + 1]);
-            imageOut.setImageResource(potenciasP[scale]);
-
-            scaleLow = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_low);
-            scaleLow.setAnimationListener(this);
-            imageOut.startAnimation(scaleLow);
-
-            scaleIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_in2);
-            scaleIn.setAnimationListener(this);
-            imageIn.startAnimation(scaleIn);
-            /*scaleUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_up);
-			scaleUp.setAnimationListener(this);
-	        imageIn = (ImageView)(findViewById(R.id.first));
-	        imageIn.startAnimation(scaleUp);
-
-	        scaleOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_out2);
-	        scaleOut.setAnimationListener(this);
-	        imageOut = (ImageView)(findViewById(R.id.second));
-	        imageOut.startAnimation(scaleOut);*/
-        }
 
     }
 
     @Override
     public void onAnimationRepeat(Animation animation) {
-        // TODO Auto-generated method stub
 
     }
 }
