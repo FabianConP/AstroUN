d962e504318fac5879e1a923ad7b018e218e6d5b
FabianConP <fabian.conpir@gmail.com>
2014-10-19 16:05:25 -0500

    Borrar clase ElementBaseAdapter repetida

--- app/src/main/java/com/unas/app/infoActivity.java
+++ app/src/main/java/com/unas/app/infoActivity.java
@@ -23,13 +23,32 @@ import modelo.Element;
 
 public class infoActivity extends Activity {
 
-    public static int scale;
     private static final int[] ARRAY_ELEMENTS_ID = {R.array.ind_0,R.array.ind_1,R.array.ind_2,R.array.ind_3};
+    public static int scale;
     private static String[] ARRAY_SCALE_NAME;
 
     private static SeekBar sbRange;
     private static TextView tvValueRange;
     private static ListView lvElements;
+
+    private static String performRange(Context context, String strScale) {
+        String[] range = strScale.split("-");
+        String from = context.getResources().getString(R.string.from);
+        String to = context.getResources().getString(R.string.to);
+        if (range.length == 1)
+            return range[0];
+        else
+            return from + " " + range[0] + " " + to + " " + range[1];
+    }
+
+    private static ArrayList<Element> getElements(Context context) {
+        String[] elementsArray = context.getResources().getStringArray(ARRAY_ELEMENTS_ID[scale]);
+        ArrayList<Element> listElements = new ArrayList<Element>();
+        for (int i = 0; i < elementsArray.length; i++)
+            listElements.add(new Element(i, elementsArray[i]));
+        return listElements;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +67,11 @@ public class infoActivity extends Activity {
         ARRAY_SCALE_NAME = getResources().getStringArray(R.array.scales);
 
         Intent intent = getIntent();
-        scale = intent.getIntExtra("SCALE",0);
+        scale = intent.getIntExtra("SCALE", 0);
 
         updateElements(getApplicationContext());
 
-        sbRange.setMax(ARRAY_ELEMENTS_ID.length-1);
+        sbRange.setMax(ARRAY_ELEMENTS_ID.length - 1);
         sbRange.setProgress(scale);
         sbRange.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -73,178 +92,89 @@ public class infoActivity extends Activity {
         });
     }
 
-    private void updateElements(Context context){
-        tvValueRange.setText(performRange(context,ARRAY_SCALE_NAME[scale]));
-        lvElements.setAdapter(new ElementBaseAdapter(context,getElements(context)));
+    private void updateElements(Context context) {
+        tvValueRange.setText(performRange(context, ARRAY_SCALE_NAME[scale]));
+        lvElements.setAdapter(new ElementBaseAdapter(context, getElements(context)));
     }
 
-    private static String performRange(Context context, String strScale){
-        String[] range = strScale.split("-");
-        String from = context.getResources().getString(R.string.from);
-        String to = context.getResources().getString(R.string.to);
-        if(range.length==1)
-            return range[0];
-        else
-            return from+" "+range[0]+" "+to+" "+range[1];
-    }
-
-    private static ArrayList<Element> getElements(Context context){
-        String[] elementsArray = context.getResources().getStringArray(ARRAY_ELEMENTS_ID[scale]);
-        ArrayList<Element> listElements = new ArrayList<Element>();
-        for (int i = 0; i<elementsArray.length;i++)
-            listElements.add(new Element(i,elementsArray[i]));
-        return listElements;
-    }
-
-    private class ElementBaseAdapter extends BaseAdapter{
-
-        private ArrayList<Element> elementList;
-
-        private LayoutInflater mInflater;
-
-        private ElementBaseAdapter(Context context, ArrayList<Element> elementList) {
-            this.elementList = elementList;
-            mInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            return elementList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return elementList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.single_row_element, null);
-                holder = new ViewHolder();
-                holder.ibDetailElement = (ImageButton) convertView.findViewById(R.id.ibDetail);
-                holder.tvNameElement = (TextView) convertView.findViewById(R.id.tvName);
-                holder.ibWikiElement = (ImageButton) convertView.findViewById(R.id.ibWiki);
-                holder.ibImagesElement = (ImageButton) convertView.findViewById(R.id.ibImages);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            Element theElement  = elementList.get(position);
-
-            holder.tvNameElement.setText(theElement.getName());
-
-            if(theElement.getWikiLink().equals("None"))
-                holder.ibWikiElement.setVisibility(View.INVISIBLE);
-            else
-                holder.ibWikiElement.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri uri = Uri.parse(elementList.get(position).getWikiLink());
-                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                        startActivity(intent);
-                    }
-                });
-            if(theElement.getImagesLink().equals("None"))
-                holder.ibImagesElement.setVisibility(View.INVISIBLE);
-            else
-                holder.ibImagesElement.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri uri = Uri.parse(elementList.get(position).getImagesLink());
-                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(intent);
-                    }
-                });
-
-            return convertView;
-        }
-
-    }
-
-    private class ElementBaseAdapter extends BaseAdapter{
-
-        private ArrayList<Element> elementList;
-
-        private LayoutInflater mInflater;
-
-        private ElementBaseAdapter(Context context, ArrayList<Element> elementList) {
-            this.elementList = elementList;
-            mInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            return elementList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return elementList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.single_row_element, null);
-                holder = new ViewHolder();
-                holder.ibDetailElement = (ImageButton) convertView.findViewById(R.id.ibDetail);
-                holder.tvNameElement = (TextView) convertView.findViewById(R.id.tvName);
-                holder.ibWikiElement = (ImageButton) convertView.findViewById(R.id.ibWiki);
-                holder.ibImagesElement = (ImageButton) convertView.findViewById(R.id.ibImages);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            Element theElement  = elementList.get(position);
-
-            holder.tvNameElement.setText(theElement.getName());
-
-            if(theElement.getWikiLink().equals("None"))
-                holder.ibWikiElement.setVisibility(View.INVISIBLE);
-            else
-                holder.ibWikiElement.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri uri = Uri.parse(elementList.get(position).getWikiLink());
-                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                        startActivity(intent);
-                    }
-                });
-            if(theElement.getImagesLink().equals("None"))
-                holder.ibImagesElement.setVisibility(View.INVISIBLE);
-            else
-                holder.ibImagesElement.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri uri = Uri.parse(elementList.get(position).getImagesLink());
-                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(intent);
-                    }
-                });
-
-            return convertView;
-        }
-
-    }    static class ViewHolder {
+    static class ViewHolder {
         ImageButton ibDetailElement;
         TextView tvNameElement;
         ImageButton ibWikiElement;
         ImageButton ibImagesElement;
+    }
+
+    protected class ElementBaseAdapter extends BaseAdapter {
+
+        private ArrayList<Element> elementList;
+
+        private LayoutInflater mInflater;
+
+        private ElementBaseAdapter(Context context, ArrayList<Element> elementList) {
+            this.elementList = elementList;
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return elementList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return elementList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.single_row_element, null);
+                holder = new ViewHolder();
+                holder.ibDetailElement = (ImageButton) convertView.findViewById(R.id.ibDetail);
+                holder.tvNameElement = (TextView) convertView.findViewById(R.id.tvName);
+                holder.ibWikiElement = (ImageButton) convertView.findViewById(R.id.ibWiki);
+                holder.ibImagesElement = (ImageButton) convertView.findViewById(R.id.ibImages);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Element theElement  = elementList.get(position);
+
+            holder.tvNameElement.setText(theElement.getName());
+
+            if(theElement.getWikiLink().equals("None"))
+                holder.ibWikiElement.setVisibility(View.INVISIBLE);
+            else
+                holder.ibWikiElement.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Uri uri = Uri.parse(elementList.get(position).getWikiLink());
+                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                        startActivity(intent);
+                    }
+                });
+            if(theElement.getImagesLink().equals("None"))
+                holder.ibImagesElement.setVisibility(View.INVISIBLE);
+            else
+                holder.ibImagesElement.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Uri uri = Uri.parse(elementList.get(position).getImagesLink());
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+
+            return convertView;
+        }
+
     }
 
 }
